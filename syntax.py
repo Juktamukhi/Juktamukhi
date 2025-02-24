336ab9332230cba0f3a036c73de0a177802dfbdf
Juktamukhi <juktamukhidey@gmail.com>
2025-02-25 00:06:35 +0530

    Add files via upload

---
+++ syntax.py
@@ -0,0 +1,17 @@
+a=7
+print(type (a))
+b="Juktamukhi"
+print(type(b))
+c=9.5
+print(type(c))
+d=True
+print(type(d))
+e=(7.8,5.6,14,"Juktamukhu")
+print(type(e))
+f=[1,"Juktamukhi",8.5]
+print(type(f))
+g={
+    "Juktamukhi":72,
+    "Juktamukhi":92
+}
+print(type(g))
